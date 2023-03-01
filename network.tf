a1c6942b6cffb04709c780863cce9b1dbf30c904
Dany <you@example.com>
2023-03-01 21:20:51 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Dany-Training/network/azurerm"
   version = "5.2.0"
-  azurerm_resource_group = "myresourcegroup"
+  esource_group_name = azurerm_resource_group.myresourcegroup.name
 }
